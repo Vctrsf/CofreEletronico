45ad3c4b8764b4bbcb84d75fa84d6b89ea0eabdf
Victor <btf.victorsf0206@gmail.com>
2023-11-07 19:20:37 -0300

    Implementado validação de texto digitado e inclusão de try catch

--- ModalGR_CofreEletronico/Program.cs
+++ ModalGR_CofreEletronico/Program.cs
@@ -2,31 +2,60 @@
 
 
 var Cofre = new CofreEletronico();
-string senhaCriptografada1; 
-string senhaCriptografada2; 
+string senhaCriptografada1;
+string senhaCriptografada2;
 string senhaCriptografada3;
-string senhaTextoPuro1; 
-string senhaTextoPuro2; 
+string senhaTextoPuro1;
+string senhaTextoPuro2;
 string senhaTextoPuro3;
 
-Cofre.ExibeTitulo();
+try
+{
 
-Console.Write("Digite a primeira senha: ");
-senhaTextoPuro1 = Console.ReadLine();
-senhaCriptografada1 = Cofre.PrimeiraCriptografia(senhaTextoPuro1);
-Console.WriteLine("Senha criptografada: " + senhaCriptografada1);
-Console.WriteLine();
+    Cofre.ExibeTitulo();
 
-Console.Write("Digite a segunda senha: ");
-senhaTextoPuro2 = Console.ReadLine();
-senhaCriptografada2 = Cofre.SegundaCriptografia(senhaTextoPuro2);
-Console.WriteLine("Senha criptografada: " + senhaCriptografada2);
-Console.WriteLine();
+    senhaTextoPuro1 = RepeteEnquantoEntradaInvalida("Digite a primeira senha");
+    senhaCriptografada1 = Cofre.PrimeiraCriptografia(senhaTextoPuro1);
+    Console.WriteLine("Senha criptografada: " + senhaCriptografada1);
+    Console.WriteLine();
 
-Console.Write("Digite a terceira senha: ");
-senhaTextoPuro3 = Console.ReadLine();
-senhaCriptografada3 = Cofre.TerceiraCriptografia(senhaTextoPuro3);
-Console.WriteLine("Senha criptografada: " + senhaCriptografada3);
+
+    senhaTextoPuro2 = RepeteEnquantoEntradaInvalida("Digite a segunda senha");
+    senhaCriptografada2 = Cofre.SegundaCriptografia(senhaTextoPuro2);
+    Console.WriteLine("Senha criptografada: " + senhaCriptografada2);
+    Console.WriteLine();
+
+
+    senhaTextoPuro3 = RepeteEnquantoEntradaInvalida("Digite a terceira senha");
+    senhaCriptografada3 = Cofre.TerceiraCriptografia(senhaTextoPuro3);
+    Console.WriteLine("Senha criptografada: " + senhaCriptografada3);
+    Console.WriteLine();
+
+
+    static string RepeteEnquantoEntradaInvalida(string mensagem)
+    {
+        bool opcaoValida = true;
+        string textoDigitado;
+        do
+        {
+            if (opcaoValida == false)
+            {
+                Console.WriteLine("\nA senha não pode ser vazia.\n");
+            }
+
+            Console.Write(mensagem + ": ");
+            textoDigitado = Console.ReadLine();
+
+            opcaoValida = !string.IsNullOrWhiteSpace(textoDigitado);
+        } while (opcaoValida == false);
+
+        return textoDigitado;
+    }
+}
+catch (Exception ex)
+{
+    Console.WriteLine("Erro: " + ex.Message);
+}
 
 
 
